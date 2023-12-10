ed5db22e2546fef0890c927243cb613d2f7249d6
Nikhil <tymnec@gmail.com>
2023-12-10 11:17:41 +0530

    marketing page complete

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import { Footer } from "./_components/footer";
+import { Navbar } from "./_components/navbar";
 
-function MarketingLayout({ children }: { children: React.ReactNode }) {
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-slate-100">
-      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <Navbar />
+      <main className="pt-40 pb-20 bg-slate-100 h-full">{children}</main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default MarketingLayout;
