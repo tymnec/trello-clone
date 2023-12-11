8be5d5f0e26a8abc77cf52c664c8a49a4eea1e81
Nikhil <tymnec@gmail.com>
2023-12-11 12:28:07 +0530

    some minor fixes - view content related

--- components/logo.tsx
+++ components/logo.tsx
@@ -12,18 +12,7 @@ export const Logo = () => {
   return (
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-        <Image
-          src="/logo.svg"
-          alt="Logo"
-          height={30}
-          width={30}
-        />
-        <p className={cn(
-          "text-lg text-neutral-700 pb-1",
-          headingFont.className,
-        )}>
-          Taskify
-        </p>
+        <Image src="/logo.svg" alt="Logo" height={50} width={80} />
       </div>
     </Link>
   );
